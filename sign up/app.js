24c1f6cb024c1a1f26be22c8a12d1e960a8ad6c4
MIRIAMDART <dartehmimi@gmsil.com>
2018-07-17 13:53:05 +0000

    new sign up added with homepage content

--- sign up/app.js
+++ sign up/app.js
@@ -1,16 +1,4 @@
-$(document).ready(function() {
+ $('.fliper-btn').click(function(){
+    $('.flip').find('.card').toggleClass('flipped');
 
-  $('#log-in-button').on('click', function() {
-    $('#log-in-button').addClass('active');
-    $('#sign-up-button').removeClass('active');
-    $('#sign-up-form').css('display', 'none');
-    $('#log-in-form').css('display', 'block');
-  });
-
-  $('#sign-up-button').on('click', function() {
-    $('#log-in-button').removeClass('active');
-    $('#sign-up-button').addClass('active');
-    $('#sign-up-form').css('display', 'block');
-    $('#log-in-form').css('display', 'none');
-  });
-})
+});
